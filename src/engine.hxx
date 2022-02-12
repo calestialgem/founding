438c4898a8daf2dda8384d2746a6c79baaa2cfae
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-13 00:25:47 +0300

    Created Engine

---
+++ src/engine.hxx
@@ -0,0 +1,80 @@
+// SPDX-FileCopyrightText: (C) 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef GECGELCEM_FOUNDING_ENGINE_HXX
+#define GECGELCEM_FOUNDING_ENGINE_HXX
+
+#include "profiler.hxx"
+#include "statistics.hxx"
+#include "timer.hxx"
+
+namespace gecgelcem::founding
+{
+	class engine final
+	{
+		public:
+
+		template<typename Updater, typename Renderer, typename Seconder>
+		inline engine(
+			double const    target_tick_rate,
+			Updater const  &updater,
+			Renderer const &renderer,
+			Seconder const &seconder) noexcept
+		{
+			timer iterations;
+			timer seconds;
+			while (running_) {
+				ticks_waiting_ += iterations.elapsed() * target_tick_rate;
+				iterations = timer{};
+
+				while (ticks_waiting_ >= 1.0) {
+					profiler profiler{tick_counter_};
+					updater(*this);
+					--ticks_waiting_;
+				}
+
+				if (rendering_) {
+					profiler profiler{frame_counter_};
+					renderer(*this);
+				}
+
+				if (seconds >= 1.0) {
+					seconds          = seconds + 1.0;
+					ticks_snapshot_  = tick_counter_;
+					frames_snapshot_ = frame_counter_;
+					tick_counter_.refresh();
+					frame_counter_.refresh();
+					seconder(*this);
+				}
+			}
+		}
+
+		constexpr void close() noexcept
+		{
+			running_ = false;
+		}
+
+		constexpr statistics const &tick_stats() const noexcept
+		{
+			return ticks_snapshot_;
+		}
+
+		constexpr statistics const &frame_stats() const noexcept
+		{
+			return frames_snapshot_;
+		}
+
+		private:
+
+		bool   running_{true};
+		bool   rendering_{true};
+		double ticks_waiting_{0.0};
+
+		statistics tick_counter_;
+		statistics frame_counter_;
+		statistics ticks_snapshot_;
+		statistics frames_snapshot_;
+	};
+} // namespace gecgelcem::founding
+
+#endif // GECGELCEM_FOUNDING_ENGINE_HXX
--- src/main.cxx
+++ src/main.cxx
@@ -2,13 +2,34 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "display.hxx"
+#include "engine.hxx"
 
 #include <GL/gl.h>
+#include <iostream>
 
 int main()
 {
-	using namespace gecgelcem::founding::display;
-	display const display{options{}};
+	using namespace gecgelcem::founding;
+	display::display const display{display::options{}};
+
+	auto const updater = [&display](engine &engine) {
+		display.update();
+		if (display.is_closed()) {
+			engine.close();
+		}
+	};
+
+	auto const renderer = [&display](engine &engine) {
+		display.show();
+	};
+
+	auto const seconder = [](engine &engine) {
+		std::cout << "Ticks:  " << engine.tick_stats().rate()
+				  << "\nFrames: " << engine.frame_stats().rate() << std::endl;
+	};
+
+	engine{20.0, updater, renderer, seconder};
+
 	while (!display.is_closed()) {
 		display.update();
 		glClearColor(display.is_focused(), display.is_iconified(), 0.0F, 1.0F);

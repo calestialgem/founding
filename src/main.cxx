65227fe8eff7517d92ee9ae3d493885a3336db73
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-12 21:24:47 +0300

    Added GLFW

--- src/main.cxx
+++ src/main.cxx
@@ -1,9 +1,16 @@
 // SPDX-FileCopyrightText: (C) 2022 Cem Geçgel <gecgelcem@outlook.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	std::cout << "Hello, world!\n";
+	glfwInit();
+	GLFWwindow *const window =
+		glfwCreateWindow(640, 480, "Founding", NULL, NULL);
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }

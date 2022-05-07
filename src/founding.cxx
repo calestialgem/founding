29d417c86f9fa72b90aa04ed7b40b31b8ea03bed
Cem GEÇGEL <gecgelcem@outlook.com>
2022-05-07 13:05:47 +0300

    Unity Build

---
+++ src/founding.cxx
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: (C) 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "display.cxx"
+#include "event.cxx"
+#include "main.cxx"
